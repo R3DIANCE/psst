aff33f0b9744f2dde4a16ec859af9743ca86e045
Rahul <rahuldottech@users.noreply.github.com>
2018-08-13 13:10:04 +0600

    Add files via upload

--- psst.php
+++ psst.php
@@ -2,7 +2,7 @@
 session_start();
 /*
 Psst File Sharer by rahuldottech
-v1.0
+v1.1
 --
 Chuck this script up on a server, configure
 options bellow, and lo and behold, you have 
@@ -47,7 +47,9 @@ enforcessl();
 
 if($_GET["logout"]){
 	$_SESSION["loggedIn"]=false;
-	}
+	header('Location: '. $_SERVER['SCRIPT_NAME']);
+
+}
 
 if(empty($_SESSION["loggedIn"])){
 	if(isset($_POST["password"])){
@@ -56,16 +58,26 @@ if(empty($_SESSION["loggedIn"])){
 		} else{
 			echo "Incorrect password! <hr>";
 		}	
-}
+	}
 }
 
 $count = 0; //Multiple File Upload Count (For Debugging)
 
 if(isset($_POST) && !empty($_FILES) && $_SERVER['REQUEST_METHOD'] == "POST"){
+	
+
+	
 	echo "<h3> Results: </h3>";
 	// Loop $_FILES to execute all files
-	foreach ($_FILES['files']['name'] as $f => $name) {     
-	    if ($_FILES['files']['error'][$f] == 4) {
+	foreach ($_FILES['files']['name'] as $f => $name) {   
+		
+		//sanitation
+		$oname2 = $name;
+		$oname = pathinfo($name, PATHINFO_FILENAME);
+		$name = htmlspecialchars($name);
+		$oname = str_replace(array('.', ',', '\\', '/', '<', '>', ' '), '' , $oname);	
+		$oname = preg_replace('/\s+/', '', $oname);
+		if ($_FILES['files']['error'][$f] == 4) {
 	        continue; // Skip file if any error found
 	    }	       
 	    if ($_FILES['files']['error'][$f] == 0) {	           
@@ -86,11 +98,11 @@ if(isset($_POST) && !empty($_FILES) && $_SERVER['REQUEST_METHOD'] == "POST"){
 	            if(move_uploaded_file($_FILES["files"]["tmp_name"][$f], $path.$name)){
 					
 				//rename file with UniqueID
-					$newname = pathinfo($name, PATHINFO_FILENAME).'-'.mt_rand(100, 9999).'.'.pathinfo($name, PATHINFO_EXTENSION);
+					$newname = $oname.'-'.mt_rand(100, 9999).'.'.pathinfo($name, PATHINFO_EXTENSION);
 					while(file_exists($path.$newname)){
-						$newname = pathinfo($name, PATHINFO_FILENAME).'-'.mt_rand(100, 9999).'.'.pathinfo($name, PATHINFO_EXTENSION);;
+						$newname = $oname.'-'.mt_rand(100, 9999).'.'.pathinfo($name, PATHINFO_EXTENSION);;
 					}
-					rename($path.$name, $path.$newname);
+					rename($path.$oname2, $path.$newname);
 	            $count++; // Number of successfully uploaded files
 				echo "\"" . $name . "\"" ." has been successfully uploaded!<br>";
 				echo "New file location: " . "<a href=\"". "http".(!empty($_SERVER['HTTPS'])?"s":"")."://".$_SERVER['SERVER_NAME'].'/'.$path.$newname . "\">" . "http".(!empty($_SERVER['HTTPS'])?"s":"")."://".$_SERVER['SERVER_NAME'].'/'.$path.$newname . '</a><br>';
@@ -115,15 +127,16 @@ function enforcessl(){
 
 
 ?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8" />
   <title>Psst File Sharer</title>
   <style>
-  body {
-	      font-family: monospace;
+	body {
+		font-family: monospace;
 
-  }
+	}
   </style>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
 <meta name="robots" content="noindex,nofollow">
@@ -135,7 +148,7 @@ function enforcessl(){
 if($_SESSION["loggedIn"] == true){
 print '
 	<h3>Upload Files</h3>
-	  <form action="" method="post" enctype="multipart/form-data">
+	  <form action="#" method="post" enctype="multipart/form-data">
 	  	<input type="file" id="file" name="files[]" multiple="multiple" accept="*" />
 	  <input type="submit" value="Upload!" />
 	</form>
@@ -145,14 +158,14 @@ print '
 ';
 } else{
 print '	<h3>Login</h3>
-	  <form action="" method="post">
+	  <form action="#" method="post">
 		Password: <input type="password" name="password"><br>
 
 	  <input type="submit" value="Login!" />
 	</form>
 
 
-	<br><br><hr><div style ="0.5em"><a href="https://github.com/rahuldottech/psst">Help/Source</a></div>
+	<br><br><hr><div style ="0.5em"><a href="https://github.com/rahuldottech/psst/">Help/Source</a></div>
 ';
 }
 ?>
