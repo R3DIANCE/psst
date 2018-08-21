7d068a6bd8ab6813f3cdbaa31e331912c192ecd2
Rahul <rahuldottech@users.noreply.github.com>
2018-08-21 18:10:08 +0600

    Update secsesh.php

--- secsesh.php
+++ secsesh.php
@@ -1,53 +1,72 @@
 <?php
+
 /*
 secSesh - PHP session handling
 by @rahuldottech
-v0.1
+
+v0.2
+
 --
+
 https://github.com/rahuldottech/
 https://rahul.tech/
 */
+
+
 //===CONFIGURATION===
+
 // Name of the session id cookie
 $GLOBALS['sessionName'] = "secID";
+
 // How long should an "idle" session remain valid 
 // for? (in seconds)
 $GLOBALS['sessionTimeout'] = 60*60*24;
+
 // How long should the session cookie remain valid 
 // for? (in seconds)
 $cookieLifetime = 4*60*60*24;
+
 // --Fingerprint Settings--
+
 	// - Use fingerprints?
 	$GLOBALS['useFingerprint'] = true;
 	// - Use user agent in fingerprint?
 	$GLOBALS['f_useUserAgent'] = true;
 	// - Use IP address in fingerprint?
 	$GLOBALS['f_useIPaddress'] = true;
+
 // --END Fingerprint Settings--
+
 //===END CONFIGURATION===
+
 ini_set( 'session.use_only_cookies', TRUE );				
 ini_set( 'session.use_trans_sid', FALSE );
 ini_set( 'session.cookie_httponly', TRUE );
 ini_set( 'session.gc_maxlifetime', $cookieLifetime );
 ini_set( 'session.cookie_lifetime', $cookieLifetime );
 ini_set( 'session.name', $sessionName );
+
 session_name($sessionName);
+
+
 function s_end(){
-	if($_SESSION["loggedIn"]){
-		$_SESSION["loggedIn"]=false;
+	if($_SESSION["s_loggedIn"]){
+		$_SESSION["s_loggedIn"]=false;
 		session_destroy();
 	}
 }
+
 function s_start(){
-	if(!$_SESSION["loggedIn"]){
+	if(!$_SESSION["s_loggedIn"]){
 		session_regenerate_id(true);
-		$_SESSION["loggedIn"] = true;
-		$_SESSION["lastActivity"] = time();
+		$_SESSION["s_loggedIn"] = true;
+		$_SESSION["s_lastActivity"] = time();
 		if($GLOBALS['useFingerprint']){
 			$_SESSION["fingerprint"] = generateFingerprint();
 		}
 	}
 }
+
 function generateFingerprint(){
 	$fingerprint = "";
 	if($GLOBALS['f_useUserAgent']){
@@ -60,19 +79,20 @@ function generateFingerprint(){
 	$fingerprint = md5($fingerprint);
 	return $fingerprint;
 }
+
 function s_check(){
-	if($_SESSION["loggedIn"]){
+	if($_SESSION["s_loggedIn"]){
 		
 		if($GLOBALS['useFingerprint'] && generateFingerprint() !== $_SESSION["fingerprint"]){
 			return false;
 		}
 		
-		if((time() - $_SESSION['lastActivity']) > $GLOBALS['sessionTimeout']){
-			$_SESSION["loggedIn"]=false;
+		if((time() - $_SESSION['s_lastActivity']) > $GLOBALS['sessionTimeout']){
+			$_SESSION["s_loggedIn"]=false;
 			session_destroy();
 			return false;
 		} else {
-			$_SESSION['lastActivity'] = time();
+			$_SESSION['s_lastActivity'] = time();
 			return true;	
 		}
 		
